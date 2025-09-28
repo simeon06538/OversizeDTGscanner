1fe20a783092165d6d4aa08ed20a02afb14de78f
simeon06538 <simeoncjorji@gmail.com>
2025-09-28 00:13:10 -0500

    idek what im doing

--- Downloads/oversizeDTGscanner/scanner.py
+++ Downloads/oversizeDTGscanner/scanner.py
@@ -1,11 +1,42 @@
 import win32com.client
+import pythoncom
+import os 
 
-bpac = win32com.client.Dispatch("bpac.Document")
-if bpac.Open(r"C:\Users\simeo\Downloads\Brother bPAC3 SDK\Templates\Item.lbx"):
-  obj = bpac.GetObject("objText")
-  if obj:
-    obj.Text = "Hello Brother!"
+def print_with_bpac(template_path, asset_data):
+  try: 
+    pythoncom.CoInitialize()
+    ObjDoc = win32com.client.Dispatch("bpac.Document")
+    bRet = ObjDoc.Open(template_path)
 
-bpac.StartPrint("",0)
-bpac.PrintOut(1,0)
- 
+    if bRet:
+      print("Template opened successfully")
+      ObjDoc.GetObject("Name").Text=asset_data.get("Name", "")
+      ObjDoc.GetObject("Section").Text= asset_data.get("Section", "")
+      ObjDoc.GetObject("Number").Text= asset_data.get("Number", "")
+
+      ObjDoc.StartPrint("",0)
+      ObjDoc.PrintOut(1,0)
+      ObjDoc.EndPrint()
+      ObjDoc.Close()
+
+      print("job sent successfully")
+    
+    else:
+      print("Failed to open template: " + template_path)
+
+  except Exception as e:
+    print("Error " + e)
+  finally:
+    pythoncom.CoUninitialize()
+        
+
+
+#bpac = win32com.client.Dispatch("bpac.Document")
+#if bpac.Open(r"C:\Users\simeo\Downloads\Brother bPAC3 SDK\Templates\Item.lbx"):
+  #obj = bpac.GetObject("objText")
+  #if obj:
+    #obj.Text = "Hello Brother!"
+
+#bpac.StartPrint("",0)
+#bpac.PrintOut(1,0)
+
