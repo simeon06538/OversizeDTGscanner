580c244c9ee7b6e16284905a802447ace4f8f648
simeon06538 <simeoncjorji@gmail.com>
2025-09-27 20:23:27 -0500

    new stuff

--- Downloads/oversizeDTGscanner/scanner.py
+++ Downloads/oversizeDTGscanner/scanner.py
@@ -1,1 +1,11 @@
-print("scanner")
+import win32com.client
+
+bpac = win32com.client.Dispatch("bpac.Document")
+if bpac.Open(r"C:\Users\simeo\Downloads\Brother bPAC3 SDK\Templates\Item.lbx"):
+  obj = bpac.GetObject("objText")
+  if obj:
+    obj.Text = "Hello Brother!"
+
+bpac.StartPrint("",0)
+bpac.PrintOut(1,0)
+ 
